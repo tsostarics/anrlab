dca0769ed368f8daaafc41742424db25c9b5ce64
Thomas Sostarics <tsostarics@gmail.com>
2020-12-27 14:48:38 -0600

    add global variable definitions for r cmd check

---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+#' 'To' 'remove' 'undefined' 'global' 'variable' 'notes..
+
+utils::globalVariables(c("age", "anrlab_instruments", "field_name", "form_name", "instrument_name",
+"instrument_prefix", "redcap_repeat_instance", "redcap_repeat_instrument",
+"report_db", "subtest", "this_inst", "tkn", "uri", "value", "where"))
