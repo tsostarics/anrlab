9c75db506ea3cc4cc9e8a1f551b60d65591811e4
Thomas Sostarics <tsostarics@gmail.com>
2021-12-01 21:47:39 -0600

    add a few var names to zzz

--- R/zzz.R
+++ R/zzz.R
@@ -3,5 +3,6 @@
 utils::globalVariables(c(
   "age", "anrlab_instruments", "field_name", "form_name", "instrument_name",
   "instrument_prefix", "redcap_repeat_instance", "redcap_repeat_instrument",
-  "report_db", "subtest", "this_inst", "tkn", "uri", "value", "where"
+  "report_db", "subtest", "this_inst", "tkn", "uri", "value", "where", "choice1",
+  "choice2", "choice3", "choice4", "number", "record_id"
 ))
